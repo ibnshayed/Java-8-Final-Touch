1c787c74265fe0681bf7fddba7f8aebe3a32fb08
ibnshayed <emranibnshayed@gmail.com>
2019-09-08 18:17:35 +0600

    ibnshayed

---
+++ LambdasAndStreams/src/LambdasAndStreams.java
@@ -0,0 +1,9 @@
+public class LambdasAndStreams {
+    public static void main(String[] args) {
+        /*
+            Book -> Java How To Program Early Edition
+            Chater -> 17 (Lambdas And Streams )
+         */
+    }
+
+}
